274a82e624795acb344878099cab19dde184240e
arantir <35970733+arantir@users.noreply.github.com>
2024-01-29 16:27:28 +0500

    Update NewAppWidget.kt

--- app/src/main/java/com/example/clockwidget/NewAppWidget.kt
+++ app/src/main/java/com/example/clockwidget/NewAppWidget.kt
@@ -1,13 +1,11 @@
 package com.example.clockwidget
 
-import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.content.Intent
+import android.os.Looper
 import android.widget.RemoteViews
 import java.text.SimpleDateFormat
-import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
@@ -30,6 +28,7 @@ class NewAppWidget : AppWidgetProvider() {
 
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
+        //Toast.makeText(context, "onEnabled", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDisabled(context: Context) {
@@ -38,6 +37,18 @@ class NewAppWidget : AppWidgetProvider() {
 
 }
 
+val handler = android.os.Handler(Looper.getMainLooper())
+/*
+val r: Runnable = object : Runnable {
+    override fun run() {
+        handler.postDelayed(this, 1000)
+        gameOver()
+    }
+}
+*/
+lateinit var views : RemoteViews
+lateinit var appWidgetManager: AppWidgetManager
+var appWidgetId: Int = 0
 
 internal fun updateAppWidget(
     context: Context,
@@ -48,21 +59,31 @@ internal fun updateAppWidget(
     //Toast.makeText(context, "---", Toast.LENGTH_SHORT).show()
 
     // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.new_app_widget)
+    //val views = RemoteViews(context.packageName, R.layout.new_app_widget)
 
+    // Текущее время
+    //views.setTextViewText(R.id.appwidget_text, SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date()))
 
-    val clockIntent = Intent(context, MainActivity::class.java)
-    val pendingIntent = PendingIntent.getActivity(context, 0, clockIntent, PendingIntent.FLAG_IMMUTABLE)
+    // Instruct the widget manager to update the widget
+    //appWidgetManager.updateAppWidget(appWidgetId, views)
+    views = RemoteViews(context.packageName, R.layout.new_app_widget)
+    com.example.clockwidget.appWidgetManager = appWidgetManager
+    com.example.clockwidget.appWidgetId = appWidgetId
 
+    setTimeLabel()
 
-    views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent)
+    handler.postDelayed(
+        {
+            setTimeLabel()
+        }, 5000)
+}
+
+fun setTimeLabel(){
 
     // Текущее время
     views.setTextViewText(R.id.appwidget_text, SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date()))
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
-
 }
-
 
